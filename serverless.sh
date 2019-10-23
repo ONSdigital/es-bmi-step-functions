179a907d59da4bfb585b8ae712b162cb5a079a34
Jordancooke <Jordanleecooke@gmail.com>
2019-10-23 15:19:35 +0100

    adapting serverless.sh file to include plugin

--- serverless.sh
+++ serverless.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 cd bmi-step-functions
+
+serverless plugin install --name serverless-step-functions
 echo Packaging serverless bundle...
 serverless package --package pkg
 echo Deploying to AWS...

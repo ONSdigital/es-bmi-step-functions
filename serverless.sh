cbc1b5295a685caf165ade851fb138125fa7ea27
Jordancooke <Jordanleecooke@gmail.com>
2019-10-24 10:32:41 +0100

    Amending ARN call to refrence resource

--- serverless.sh
+++ serverless.sh
@@ -3,7 +3,6 @@
 cd bmi-step-functions
 
 serverless plugin install --name serverless-step-functions
-serverless plugin install --name serverless-pseudo-parameters
 echo Packaging serverless bundle...
 serverless package --package pkg
 echo Deploying to AWS...

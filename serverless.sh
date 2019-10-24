0d3868a88d0dd5043d8b9220989b390113f76c8e
Jordancooke <Jordanleecooke@gmail.com>
2019-10-24 12:34:24 +0100

    Reverting to string arn and adding psuedo

--- serverless.sh
+++ serverless.sh
@@ -3,6 +3,7 @@
 cd bmi-step-functions
 
 serverless plugin install --name serverless-step-functions
+serverless plugin install --name serverless-pseudo-parameters
 echo Packaging serverless bundle...
 serverless package --package pkg
 echo Deploying to AWS...

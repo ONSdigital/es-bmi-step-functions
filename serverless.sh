52779328197a871cef51e2aefb2181756143e2f4
Jordancooke <Jordanleecooke@gmail.com>
2020-02-12 12:19:54 +0000

    test_bricks_serverless.yml
    no changes added to commit (use "git add" and/or "git commit -a")
    Updating Step function scripts
    
    - Implementing Blocks step function script.
    - Implementing Bricks seperate step function script.
    - Implementing SG seperate step function script.
    - Updating serverless.sh to reflect changes to independant scripts.
    - Updating serverless_remove.sh to refelect changes.
    - Overall Bug fixes/ Improvements accross step functions.

--- serverless.sh
+++ serverless.sh
@@ -3,7 +3,9 @@
 cd step-function-deploy-repository
 
 serverless plugin install --name serverless-step-functions
-echo Deploying Sand and Gravel to AWS..
+echo Deploying Sand and Gravel to AWS...
 serverless deploy --config sg_serverless.yml --verbose;
-echo Deploying Blocks to AWS..
+echo Deploying Blocks to AWS...
 serverless deploy --config blk_serverless.yml --verbose;
+echo Deploying Bricks to AWS...
+serverless deploy --config brk_serverless.yml --verbose;

3e31d81fda692a5f873cde55ce6378460e8dcce5
Michael Candlin <29595780+kingmushroom@users.noreply.github.com>
2020-03-10 12:31:14 +0000

    Update serverless.sh

--- serverless.sh
+++ serverless.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 cd es-bmi-step-functions-deploy-repository
-export SLS_DEBUG=1
 serverless plugin install --name serverless-step-functions
 echo Deploying Sand and Gravel to AWS...
-serverless print --config sg_serverless.yml
 serverless deploy --config sg_serverless.yml --verbose;
 echo Deploying Blocks to AWS...
 serverless deploy --config blk_serverless.yml --verbose;
